b323cd86a426cd72b10abb95e1cbb7d456718fc8
Yumiria <154666142+Yumiara@users.noreply.github.com>
2023-12-23 19:56:23 +0700

    Create Version.lua

---
+++ Version.lua
@@ -0,0 +1,8 @@
+_G.Version = {
+    ["The Mimic"] = "UPV1D",
+    ["Dragon Adventure"] = "UPV1D",
+    ["Blox Fruit"] = "UPV1D",
+    ["Pet Simulator 99"] = "UPV1D",
+    ["Weird Stitch Dad"] = "UPV1D",
+    ["Tower Of Hell"] = "UPV1D",
+}
