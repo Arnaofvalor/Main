19ce00ab4c920e148da2844af7fd0c22c0c3ccc2
Yumiria <154666142+Yumiara@users.noreply.github.com>
2023-12-24 16:41:31 +0700

    Update DeveloperTools.lua

--- DeveloperTools.lua
+++ DeveloperTools.lua
@@ -6,6 +6,28 @@ local Gravity = false
 local originalGravity = game.workspace.Gravity
 local originalWalkSpeed = game.Players.LocalPlayer.Character.Humanoid.WalkSpeed
 local originalJumpPower = game.Players.LocalPlayer.Character.Humanoid.JumpPower
+local Setwalkspeed = nil
+local Setjumppower = nil
+local walkspeedrequest = false
+local jumppowerrequest = false
+game:GetService("RunService").Stepped:connect(
+    function()
+        if walkspeedrequest then
+            game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Setwalkspeed
+        else
+            game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = originalWalkSpeed
+        end
+    end
+)
+game:GetService("RunService").Stepped:connect(
+    function()
+        if jumppowerrequest then
+            game.Players.LocalPlayer.Character.Humanoid.JumpPower = Setjumppower
+        else
+            game.Players.LocalPlayer.Character.Humanoid.JumpPower = originalJumpPower
+        end
+    end
+)
 local Part = Instance.new("Part")
 Part.Size = Vector3.new(2, 0.2, 1.5)
 Part.Material = Enum.Material.Grass
@@ -589,6 +611,282 @@ do
     Tabs.Developer:AddSection("Developer Bypass")
     Tabs.Developer:AddSection("Developer Clipboard")
     Tabs.Developer:AddSection("Developer Tools")
+    Tabs.Developer:AddButton({
+        Title = "Position Finder",
+        Description = "GUI | SetClipboard | Ctrl + C",
+        Callback = function()            
+            -- Made by TheXploiter
+ 
+            local ScreenGui = Instance.new("ScreenGui")
+            local Frame = Instance.new("Frame")
+            local title = Instance.new("TextLabel")
+            local copy = Instance.new("TextButton")
+            local pos = Instance.new("TextBox")
+            local find = Instance.new("TextButton")
+            
+            --Properties:
+            
+            ScreenGui.Parent = game.CoreGui
+            ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+            
+            Frame.Parent = ScreenGui
+            Frame.BackgroundColor3 = Color3.fromRGB(43, 43, 43)
+            Frame.BorderSizePixel = 0
+            Frame.Position = UDim2.new(0.639646292, 0, 0.399008662, 0)
+            Frame.Size = UDim2.new(0, 387, 0, 206)
+            Frame.Active = true
+            
+            title.Name = "title"
+            title.Parent = Frame
+            title.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+            title.BorderSizePixel = 0
+            title.Size = UDim2.new(0, 387, 0, 50)
+            title.Font = Enum.Font.GothamBold
+            title.Text = "Position Finder"
+            title.TextColor3 = Color3.fromRGB(255, 255, 255)
+            title.TextSize = 30.000
+            title.TextWrapped = true
+            
+            copy.Name = "copy"
+            copy.Parent = Frame
+            copy.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+            copy.BorderSizePixel = 0
+            copy.Position = UDim2.new(0.527131796, 0, 0.635922313, 0)
+            copy.Size = UDim2.new(0, 148, 0, 50)
+            copy.Font = Enum.Font.GothamSemibold
+            copy.Text = "Copy"
+            copy.TextColor3 = Color3.fromRGB(255, 255, 255)
+            copy.TextSize = 20.000
+            
+            pos.Name = "pos"
+            pos.Parent = Frame
+            pos.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+            pos.BorderSizePixel = 0
+            pos.Position = UDim2.new(0.0904392749, 0, 0.305825233, 0)
+            pos.Size = UDim2.new(0, 317, 0, 50)
+            pos.Font = Enum.Font.GothamSemibold
+            pos.Text = ""
+            pos.TextColor3 = Color3.fromRGB(255, 255, 255)
+            pos.TextSize = 14.000
+            pos.TextWrapped = true
+            
+            find.Name = "find"
+            find.Parent = Frame
+            find.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+            find.BorderSizePixel = 0
+            find.Position = UDim2.new(0.0904392898, 0, 0.635922313, 0)
+            find.Size = UDim2.new(0, 148, 0, 50)
+            find.Font = Enum.Font.GothamSemibold
+            find.Text = "Find"
+            find.TextColor3 = Color3.fromRGB(255, 255, 255)
+            find.TextSize = 20.000
+            
+            -- Scripts:
+            
+            local function UMTQ_fake_script() -- copy.LocalScript 
+                local script = Instance.new('LocalScript', copy)
+            
+                script.Parent.MouseButton1Click:Connect(function()
+                    setclipboard(script.Parent.Parent.pos.Text)
+                end)
+            end
+            coroutine.wrap(UMTQ_fake_script)()
+            local function KJAYG_fake_script() -- Frame.Dragify 
+                local script = Instance.new('LocalScript', Frame)
+            
+                local UIS = game:GetService("UserInputService")
+                function dragify(Frame)
+                    dragToggle = nil
+                    local dragSpeed = 0
+                    dragInput = nil
+                    dragStart = nil
+                    local dragPos = nil
+                    function updateInput(input)
+                        local Delta = input.Position - dragStart
+                        local Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + Delta.X, startPos.Y.Scale, startPos.Y.Offset + Delta.Y)
+                        game:GetService("TweenService"):Create(Frame, TweenInfo.new(0.25), {Position = Position}):Play()
+                    end
+                    Frame.InputBegan:Connect(function(input)
+                        if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) and UIS:GetFocusedTextBox() == nil then
+                            dragToggle = true
+                            dragStart = input.Position
+                            startPos = Frame.Position
+                            input.Changed:Connect(function()
+                                if input.UserInputState == Enum.UserInputState.End then
+                                    dragToggle = false
+                                end
+                            end)
+                        end
+                    end)
+                    Frame.InputChanged:Connect(function(input)
+                        if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+                            dragInput = input
+                        end
+                    end)
+                    game:GetService("UserInputService").InputChanged:Connect(function(input)
+                        if input == dragInput and dragToggle then
+                            updateInput(input)
+                        end
+                    end)
+                end
+            
+                dragify(script.Parent)
+            end
+            coroutine.wrap(KJAYG_fake_script)()
+            local function EKBNYI_fake_script() -- find.LocalScript 
+                local script = Instance.new('LocalScript', find)
+            
+                script.Parent.MouseButton1Down:Connect(function()
+                    script.Parent.Parent.pos.Text = tostring(game.Players.LocalPlayer.Character.HumanoidRootPart.Position)
+                end)
+            end
+            coroutine.wrap(EKBNYI_fake_script)()
+        end
+    })
+    Tabs.Developer:AddButton({
+        Title = "Infinite Yield",
+        Description = "loadstring | Gui",
+        Callback = function()            
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+        end
+    })
+    Tabs.Developer:AddButton({
+        Title = "Dex",
+        Description = "loadstring | Gui | Pc | Mobile (70%)",
+        Callback = function()            
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/infyiff/backup/main/dex.lua"))()
+        end
+    })
+    Tabs.Developer:AddButton({
+        Title = "Remote Spy",
+        Description = "loadstring | Gui | Unknow Device",
+        Callback = function()            
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/infyiff/backup/main/SimpleSpyV3/main.lua"))()
+        end
+    })
+    Tabs.Developer:AddButton({
+        Title = "Remote Spy",
+        Description = "loadstring | Gui | Unknow Device | Hydroxide",
+        Callback = function()            
+            local owner = "Upbolt"
+            local branch = "revision"
+ 
+            local function webImport(file)
+                return loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/Hydroxide/%s/%s.lua"):format(owner, branch, file)), file .. '.lua')()
+            end
+ 
+            webImport("init")
+            webImport("ui/main")
+        end
+    })
+    Tabs.Developer:AddButton({
+        Title = "Audio logger",
+        Description = "loadstring | Gui | Pc & Mobile",
+        Callback = function()            
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/infyiff/backup/main/dex.lua"))()
+        end
+    })
+    Tabs.Developer:AddButton({
+        Title = "BTools",
+        Description = "loadstring | Gui | Pc & Mobile",
+        Callback = function()            
+            local player = game.Players.LocalPlayer
+            local mouse = player:GetMouse()
+            
+            -- Objects
+            
+            local ScreenGui = Instance.new("ScreenGui")
+            local TextButton = Instance.new("TextButton")
+            local On = Instance.new("StringValue")
+            
+            -- Properties
+            
+            ScreenGui.Parent = player.PlayerGui
+            
+            TextButton.Parent = ScreenGui
+            TextButton.BackgroundColor3 = Color3.new(0.784314, 0.784314, 0.784314)
+            TextButton.BorderSizePixel = 0
+            TextButton.Position = UDim2.new(0, 0, 0.455743879, 0)
+            TextButton.Size = UDim2.new(0, 186, 0, 35)
+            TextButton.Font = Enum.Font.SourceSans
+            TextButton.Text = "Btools (Off)"
+            TextButton.TextColor3 = Color3.new(0.27451, 0.27451, 0.27451)
+            TextButton.TextScaled = true
+            TextButton.TextSize = 14
+            TextButton.TextWrapped = true
+            
+            
+            On.Parent = TextButton
+            On.Value = "Off"
+            
+            -- Scripts 
+            
+            TextButton.MouseButton1Up:Connect(function()
+                if On.Value == "Off" then
+                    On.Value = "On"
+                    TextButton.Text = "Btools (On)"
+                else
+                    On.Value = "Off"
+                    TextButton.Text = "Btools (Off)"
+                end
+            end)
+            
+            mouse.Button1Up:Connect(function()
+                if On.Value == "Off" then
+                    print('btools off')
+                else
+                    if mouse.Target.Locked == true then
+                        mouse.Target:Destroy()
+                    else
+                        mouse.Target:Destroy()
+                    end
+                end
+            end)
+        end
+    })
+    local SliderW = Tabs.Developer:AddSlider("Slider", {
+        Title = "WalkSpeed",
+        Description = "",
+        Default = originalWalkSpeed,
+        Min = 0,
+        Max = 5,
+        Rounding = 1,
+        Callback = function(Value)
+
+        end
+    })
+    SliderW:OnChanged(function(Value)
+        Setwalkspeed = Value
+    end)
+    local SliderJ = Tabs.Developer:AddSlider("Slider", {
+        Title = "WalkSpeed",
+        Description = "",
+        Default = originalJumpPower,
+        Min = 0,
+        Max = 5,
+        Rounding = 1,
+        Callback = function(Value)
+
+        end
+    })
+    SliderJ:OnChanged(function(Value)
+        Setjumppower = Value
+    end)
+    local Toggle = Tabs.PlayerTab:AddToggle("MyToggle", {Title = "Enabled/Disabled WalkSpeed", Default = false })
+
+    Toggle:OnChanged(function()
+        walkspeedrequest = Options.MyToggle.Value
+    end)
+    local Toggle = Tabs.PlayerTab:AddToggle("MyToggle", {Title = "Enabled/Disabled JumpPower", Default = false })
+
+    Toggle:OnChanged(function()
+        jumppowerrequest = Options.MyToggle.Value
+    end)
+    local Toggle = Tabs.Developer:AddToggle("MyToggle3", {Title = "Float", Default = false })
+
+    Toggle:OnChanged(function()
+        Float = Options.MyToggle3.Value
+    end)
     Tabs.Developer:AddSection("Setting")
     local Toggle = Tabs.Developer:AddToggle("MyToggle", {Title = "Far", Default = false })
 
@@ -599,11 +897,6 @@ do
 
     Toggle:OnChanged(function()
         UFast = Options.MyToggle2.Value
-    end)
-    local Toggle = Tabs.Developer:AddToggle("MyToggle3", {Title = "Float", Default = false })
-
-    Toggle:OnChanged(function()
-        Float = Options.MyToggle3.Value
     end)
     local Toggle = Tabs.Developer:AddToggle("MyToggle4", {Title = "Gravity 0", Default = false })
 
