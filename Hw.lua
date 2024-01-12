737b62f53d78064896ce73ad3d9f547bfd29da68
AdminEvilDRagon <92892215+AdminEvilDRagon@users.noreply.github.com>
2024-01-12 15:45:32 +0100

    Hw.lua

--- Hw.lua
+++ Hw.lua
@@ -1,3 +1,6 @@
-"", -- 123-456-789 / PERM
-"", -- 123-456-789 / PERM
-""123-456-789", -- test / PERM", -- test / PERM
+_G.Hwid = {
+    "03a18606-3dda-45ca-bc6d-d5b1cb22c9c9", --Xen / PERM
+    "5792f0ac-f1ea-47a9-bd7e-3c99f5978dfa", --kazu / PERM
+    "a431a695-adc4-4411-a2ad-7343e0f1c1fe", --baqer / PERM
+    "9f5f1a97-9aae-4154-b49d-925d2fdc1d45", -- yuhabah / PERM
+}
