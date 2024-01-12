b9cd2ff31f08e3d38de73689c4ee1d6c0d2459ce
Yumiria <154666142+Yumiara@users.noreply.github.com>
2024-01-12 18:34:13 +0700

    Update Hw.lua

--- Hw.lua
+++ Hw.lua
@@ -1,5 +1,5 @@
 _G.Hwid = {
-    ["3483-wadw-WJEw-32fF"] = true, 
-    ["hwid should be here"] = true,
-    ["add more here"] = true,
+    "03a18606-3dda-45ca-bc6d-d5b1cb22c9c9", --Xen / PERM
+    "v",
+    "b",
 }
