2e81e5df5d745aed252bb7e72e1f88be91aa20bf
Yumiria <154666142+Yumiara@users.noreply.github.com>
2024-01-13 13:48:10 +0700

    Update Hw.lua

--- Hw.lua
+++ Hw.lua
@@ -3,5 +3,9 @@ _G.Hwid = {
     "5792f0ac-f1ea-47a9-bd7e-3c99f5978dfa", --kazu / PERM
     "a431a695-adc4-4411-a2ad-7343e0f1c1fe", --baqer / PERM
     "9f5f1a97-9aae-4154-b49d-925d2fdc1d45", -- yuhabah / PERM
+    "13d70443-3365-4d98-a855-4f06e276d38c", -- Pann / PERM
+    "9df8a9d4-2eaa-4f17-ae8c-baec2d451cc8", -- fitzz / PERM
+    "b1910b58-4435-49cd-b097-6a0107e084e4", -- Zum / PERM
+    "6c3080df-a052-45ed-b473-4541b081fcd7", -- keng / 13-02
+    "c46edf57-c594-4e97-8d9e-1a90aa6244ef", -- Luffy / 23-02
 }
-123-456-789
