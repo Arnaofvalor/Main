21b15584d712fc46d3b2980ef143bbfe59991d31
Yumiria <154666142+Yumiara@users.noreply.github.com>
2024-01-18 18:12:50 +0700

    Update Hw.lua

--- Hw.lua
+++ Hw.lua
@@ -3,15 +3,16 @@ _G.Hwid = {
     "5792f0ac-f1ea-47a9-bd7e-3c99f5978dfa", --kazu / PERM
     "9ef4ef4c-dcbe-457f-b2ab-e74ae9b7bf73", --baqer / PERM
     "9f5f1a97-9aae-4154-b49d-925d2fdc1d45", -- yuhabah / PERM
-    "13d70443-3365-4d98-a855-4f06e276d38c", -- Pann / PERM
+    "0ec2994d-23d4-4b5d-8f56-ff023682a0b8", -- Pann / PERM
     "9df8a9d4-2eaa-4f17-ae8c-baec2d451cc8", -- fitzz / PERM
     "b1910b58-4435-49cd-b097-6a0107e084e4", -- Zum / PERM
-    "6c3080df-a052-45ed-b473-4541b081fcd7", -- keng / 13-02
+    "43b97b5e-6ab9-4801-83b3-72148701c8f6", -- keng / 13-02
     "c46edf57-c594-4e97-8d9e-1a90aa6244ef", -- Luffy / 23-02
     "0f70d80a-4d70-4424-92a0-2ed62dea7328", -- Ferris / 23-02
     "bc476849-c211-4737-88f4-d12c7a19bfbB", --Denial / PERM
     "3d836cb8-554a-41a3-ba87-aa089664099f", --Bao / PERM
-    "5a7d3235-f262-4ac0-9f8b-ea4298095448", --Pannisnitro / 13/03
+    "8f6cf75a-df7a-4b36-a50d-b320eab1a7e9", --Pannisnitro / 13/03
     "0c40fd33-0282-4559-85c5-e7e5ec81f55e", --EvilDragon / Admin
     "afbc9464-f052-47ec-8641-8e67da0f9547", --RainysSix / 14/02
+    "3c327731-b1cd-4d0d-a1de-aa62c4b3bab2", --ABC / PERM
 }
